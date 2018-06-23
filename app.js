e1e2806280c299dd6332407b9c1bad951a1b6885
Jimmy Roberts <jimmy.roberts09@outlook.com>
2018-06-23 22:30:59 +0100

    smooth arrow key movement done

--- app.js
+++ app.js
@@ -1,50 +1,88 @@
 var x = 0;
-var viewportWidth = 500;
-setViewportWidth();
+var y = 0;
+var arrowRightPressed = false;
+var arrowLeftPressed = false;
+var arrowUpPressed = false;
+var arrowDownPressed = false;
+var speedIncrement = 5;
+setViewport();
+updatePosition();
+setInterval (function(){
+  if (arrowRightPressed)
+    x = x + speedIncrement;
+  if (arrowLeftPressed)
+    x = x - speedIncrement;
+  if (arrowUpPressed)
+    y = y - speedIncrement;
+  if (arrowDownPressed)
+    y = y + speedIncrement;
+  updatePosition();
+}, 10);
 
-function setViewportWidth() {
+function setViewport() {
   viewportWidth = document.documentElement.clientWidth;
+  viewportHeight = document.documentElement.clientHeight;
   var style = {
-    width: viewportWidth + "px"
+    width: viewportWidth + "px",
+    height: viewportHeight + "px"
   };
   $("#page").css(style);
 }
 
-function moveRight() {
-    x = x + 5;
-    updatePosition();
-}
-
-function moveLeft() {
-    x = x - 5;
-    updatePosition();
-}
-
 function updatePosition() {
-
     if (x > viewportWidth)
         x = -150;
     if (x < -150)
         x = viewportWidth;
+    if (y > viewportHeight)
+        y = -110;
+    if (y < -110)
+        y = viewportHeight;
 
-    var style = {left:x};
+    var style = {left: x, top: y};
     $("#car").css(style);
 }
 
 window.addEventListener("keydown", function(event){
     switch(event.key) {
         case "ArrowRight":
-            moveRight();
-            event.preventDefault();
+            arrowRightPressed = true;
             break;
-
         case "ArrowLeft":
-            moveLeft();
-            event.preventDefault();
+            arrowLeftPressed = true;
             break;
+        case "ArrowUp":
+            arrowUpPressed = true;
+            break;
+        case "ArrowDown":
+            arrowDownPressed = true;
+            break;
+        default:
+            return;
     };
+    event.preventDefault();
+}, true);
+
+window.addEventListener("keyup", function(event){
+    switch(event.key) {
+        case "ArrowRight":
+            arrowRightPressed = false;
+            break;
+        case "ArrowLeft":
+            arrowLeftPressed = false;
+            break;
+        case "ArrowUp":
+            arrowUpPressed = false;
+            break;
+        case "ArrowDown":
+            arrowDownPressed = false;
+            break;
+        default:
+            return;
+    };
+    event.preventDefault();
 }, true);
 
 window.addEventListener("resize", function(event){
-  setViewportWidth();
+  setViewport();
 }, true);
